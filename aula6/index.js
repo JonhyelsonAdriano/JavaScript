8c78996337980c45aa5ac463c2d937e0d97ab3ff
Adriano Dantas <adrianodantas1911@gmail.com>
2024-12-09 23:03:15 -0300

    aprandendo usar constantes em JavaScript

--- aula6/index.js
+++ aula6/index.js
@@ -1,5 +1,17 @@
-console.log('Joao nasceu em 1984');
-console.log('Em 200 Joao conheceu Maria');
-console.log('Joao casou-se com Maria em 2012.');
-console.log('Maria teve 1 filho com joao em 2015'); 
-console.log('O filho de joao se chamar Eduardo.')
+let nome = 'Adriano Dantas'; //STRING
+
+
+// NAO PODEMOS CRIAR VARIAVEIS COM PALAVRAS RESERVADAS
+// VARIAVEIS PRECISAM TER NOMES SIGNIFICATIVOS
+// NAO PODE COMECAR O NOME DE UMA VARIAVEL COM UM NUMERO
+// NAO PODEM CONTER ESPACOS OU TRACOS
+// UTILIZAMOS camelCase
+// Case-sensitive
+// NAO PODEMOS REDECLARAR VARIAVEIS COM let
+// NAO UTILIZE VAR, UTILIZE LET.
+
+console.log(nome,' nasceu em 1984');
+console.log('Em 200', nome, 'conheceu Maria');
+console.log(nome,' casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com,nome',nome, 'em 2015'); 
+console.log('O filho de' ,nome, 'se chamar Eduardo.');
---
+++ aula7/index.js
@@ -0,0 +1,15 @@
+// NAO PODEMOS CRIAR contanstes COM PALAVRAS RESERVADAS
+// Constantes PRECISAM TER NOMES SIGNIFICATIVOS
+// NAO PODE COMECAR O NOME DE UMA Constante COM UM NUMERO
+// NAO PODEM CONTER ESPACOS OU TRACOS
+// UTILIZAMOS camelCase
+// Case-sensitive
+// NAO PODE MODIFICAR O VALOR DE UMA CONSTANTE
+// NAO UTILIZE VAR, UTILIZE CONST.
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado +5;
+console.log(resultadoTriplicado);
